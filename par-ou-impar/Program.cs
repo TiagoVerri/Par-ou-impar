5d4e6ba51a9cdd4f32e99f574ccf3ba24561b431
Tiago Verri <tiagov.verri@gmail.com>
2023-08-20 22:34:12 -0300

    primeiro commit

---
+++ par-ou-impar/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("PAR ou IMPAR \n");
+
+Console.WriteLine("Digite um número");
+
+double num = double.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("O número " + num + " é par \n");
+}
+else
+{
+    Console.WriteLine("O número " + num + " é impar \n");
+}
